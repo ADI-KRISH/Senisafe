dad08eb6391ba5050173439ce63a0581ce64fbee
GS ADITHYA KRISHNA <143203146+1234ak1234@users.noreply.github.com>
2025-05-16 14:36:32 +0530

    Initial commit

---
+++ lib/screens/home/home_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:senisafe/main.dart';
+void main(){
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home:Scaffold((
+        backgroundColor : Colors.grey[200] ,
+        body:  Stack(
+      children: [
+        Positioned(top : 20,
+        left: 20,
+        child: Container(
+          width: 200,
+          height: 150,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius : BorderRadius.circular(8)
+          ),
+        ),)
+      ]
+    ))))
+  }
+    throw UnimplementedError();
+  }
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,34 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import agora_rtc_engine
+import cloud_firestore
+import device_info_plus
+import firebase_auth
+import firebase_core
+import firebase_messaging
+import flutter_local_notifications
+import geolocator_apple
+import iris_method_channel
+import path_provider_foundation
+import shared_preferences_foundation
+import url_launcher_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AgoraRtcNgPlugin.register(with: registry.registrar(forPlugin: "AgoraRtcNgPlugin"))
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  IrisMethodChannelPlugin.register(with: registry.registrar(forPlugin: "IrisMethodChannelPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+}
